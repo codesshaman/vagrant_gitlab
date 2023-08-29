1b3b6bdd1fd31ce4d0691534c78265637dcae266
Jimmie Leslee <jleslee@va-b5.kzn.21-school.ru>
2023-08-29 13:35:06 +0300

    install gitlab registry

--- gitlab_setup.sh
+++ gitlab_setup.sh
@@ -42,22 +42,45 @@ apt update && apt install -y \
     make \
     perl \
     htop \
-    # postfix \
+    gitlab-ce \
     openssh-server \
     ca-certificates
 
-EXTERNAL_URL="http://$1"
+sed -i "s!external_url 'http://gitlab.example.com'!external_url 'http://$1'!1" /etc/gitlab/gitlab.rb
 
-apt install gitlab-ce
+sed -i "s!gitlab_rails['registry_enabled'] = true!In the bottom!1" /etc/gitlab/gitlab.rb
+sed -i "s!registry_external_url 'https://registry.example.com'!In the bottom!1" /etc/gitlab/gitlab.rb
+sed -i "s!registry_nginx['enable'] = false!In the bottom!1" /etc/gitlab/gitlab.rb
+sed -i "s!registry_nginx['proxy_set_headers'] = {! !1" /etc/gitlab/gitlab.rb
+sed -i 's!"Host" => "$http_host",! !1' /etc/gitlab/gitlab.rb
+sed -i 's!"X-Real-IP" => "$remote_addr",! !1' /etc/gitlab/gitlab.rb
+sed -i 's!"X-Forwarded-For" => "$proxy_add_x_forwarded_for",! !1' /etc/gitlab/gitlab.rb
+sed -i 's!"X-Forwarded-Proto" => "https",! !1' /etc/gitlab/gitlab.rb
+sed -i 's!"X-Forwarded-Ssl" => "on"! !1' /etc/gitlab/gitlab.rb
+sed -i "s!registry_nginx['listen_port'] = 5050!In the bottom!1" /etc/gitlab/gitlab.rb
 
-echo -e "${warn}[k8s installer]${no} ${cyan}Установка mkcert для самоподписных сертификатов${no}"
-curl -s https://api.github.com/repos/FiloSottile/mkcert/releases/latest| grep browser_download_url  | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
-mv mkcert-v*-linux-amd64 mkcert
-chmod a+x mkcert
-mv mkcert /usr/local/bin/
+
+echo "gitlab_rails['registry_enabled'] = true" >> /etc/gitlab/gitlab.rb
+echo "registry_external_url 'http://registry.$1'" >> /etc/gitlab/gitlab.rb
+echo "registry_nginx['enable'] = true" >> /etc/gitlab/gitlab.rb
+echo "registry_nginx['proxy_set_headers'] = {" >> /etc/gitlab/gitlab.rb
+echo '  "Host" => "$http_host",' >> /etc/gitlab/gitlab.rb
+echo '  "X-Real-IP" => "$remote_addr",' >> /etc/gitlab/gitlab.rb
+echo '  "X-Forwarded-For" => "$proxy_add_x_forwarded_for",' >> /etc/gitlab/gitlab.rb
+echo '  "X-Forwarded-Proto" => "http",' >> /etc/gitlab/gitlab.rb
+echo '  "X-Forwarded-Ssl" => "off"' >> /etc/gitlab/gitlab.rb
+echo "}" >> /etc/gitlab/gitlab.rb
+echo "registry_nginx['listen_port'] = 5050" >> /etc/gitlab/gitlab.rb
+echo "registry_nginx['listen_https'] = false" >> /etc/gitlab/gitlab.rb
 
 gitlab-ctl reconfigure
 
 gitlab-ctl restart
 
 su - vagrant -c "sudo cat /etc/gitlab/initial_root_password | grep Password: | sed -r 's/.{,10}//' > ~/gitlab.passwd"
+
+echo -e "${warn}[k8s installer]${no} ${cyan}Установка mkcert для самоподписных сертификатов${no}"
+curl -s https://api.github.com/repos/FiloSottile/mkcert/releases/latest| grep browser_download_url  | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
+mv mkcert-v*-linux-amd64 mkcert
+chmod a+x mkcert
+mv mkcert /usr/local/bin/
